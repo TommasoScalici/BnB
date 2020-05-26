6bc330d4be6897e4fd89e2b1bcf69f8a2c14925e
Salvatore Apicella <salvatore.apicella@outlook.com>
2020-05-26 19:13:39 +0200

    26/05/2020

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,8 +1,13 @@
+const userController = require("../controllers/user.controller.js");
+const apartmentController = require("../controllers/apartments.controller.js");
+const Apartment = require('../models/apartment.js');
+const stream = require('stream');
 module.exports = function(app, express) {
 
     require('../routes/user.routes.js')(app);
+    require('../routes/apartment.routes.js')(app);
 
-    // mi serve per testare
+    //mi serve per testare
     var apartments = [
         {
             name: "Appartamento 1",
@@ -17,7 +22,7 @@ module.exports = function(app, express) {
         {
             name: "Appartamento 3",
             price: 25.50,
-            description: "Bellissimo appartamento in centro città, vicino la stazione, ecc.",
+            description: "Bellissimo appartamento in centro città, vicno alla stazione, ecc.",
         },
         {
             name: "Appartamento 4",
@@ -27,15 +32,52 @@ module.exports = function(app, express) {
     ];
     // fine roba di test
 
+    // var stream = Apartment.find().cursor()
+    // cursor.on('error', function (err) {
+    // console.error(err)
+    // })
+    // stream.on('data', function (doc) {
+    //  console.log(doc)
+    // })
+    
+    // Apartment.find().stream()
+    // .on('data', function(doc){
+    //   apartments.push(convert(doc));
+    // })
+    // .on('error', function(err){
+    //   // handle error
+    // })
+    // .on('end', function(){
+    //     cb (null, emotions)
+    // });
+
+
+    const apartment =  Apartment.find(function (err, apartment) {
+        if (err) return console.error(err);
+        console.log(apartment);
+        return
+      })
+
+      const list = apartment.exec();
+        console.log(list);
+
     app.get('/', function(req, res) {
-        res.render("index", {pagetitle: "Home", path: "home", apartments: apartments});
+        var name = req.body.name;
+        res.render("index", {pagetitle: "Home", apartments: apartments});
     });
 
-    app.get('/logout', function(req, res) {
-        res.redirect('/api/users/logout');
+
+    app.get('/signin', function(req, res) {
+        res.render("signin");
     });
 
     app.get('/signup', function(req, res) {
-        res.render("index", {pagetitle: "Registrazione", path: "signup"});
+        res.render("signup");
     });
+
+    app.get("/insert", function(req,res)
+    {
+        res.render("insertApartment");
+    });
+
 };
