0e97d68ee86c8e0eb2d8535ca2fcc7f981e192f9
Salvatore Apicella <salvatore.apicella@outlook.com>
2020-05-27 12:12:34 +0200

    Apartment Form

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -5,63 +5,12 @@ module.exports = function(app, express) {
     require('../routes/user.routes.js')(app);
     require('../routes/apartment.routes.js')(app);
 
-    // mi serve per testare
-    var apartments = [
-        {
-            name: "Appartamento 1",
-            price: 15.50,
-            description: "Bellissimo appartamento vicino al mare",
-        },
-        {
-            name: "Appartamento 2",
-            price: 22,
-            description: "Bellissimo appartamento in montagna",
-        },
-        {
-            name: "Appartamento 3",
-            price: 25.50,
-            description: "Bellissimo appartamento in centro città, vicino la stazione, ecc.",
-        },
-        {
-            name: "Appartamento 4",
-            price: 19.90,
-            description: "Casa di Pirrone",
-        },
-    ];
 
-    // fine roba di test
-
-    // var stream = Apartment.find().cursor()
-    // cursor.on('error', function (err) {
-    // console.error(err)
-    // })
-    // stream.on('data', function (doc) {
-    //  console.log(doc)
-    // })
-    
-    // Apartment.find().stream()
-    // .on('data', function(doc){
-    //   apartments.push(convert(doc));
-    // })
-    // .on('error', function(err){
-    //   // handle error
-    // })
-    // .on('end', function(){
-    //     cb (null, emotions)
-    // });
-
-
-    const apartment =  Apartment.find(function (err, apartment) {
-        if (err) return console.error(err);
-        console.log(apartment);
-        return
-      });
-
-    const list = apartment.exec();
-        console.log(list);
-
-    app.get('/', function(req, res) {
-        res.render("index", {pagetitle: "Home", path: "home", apartments: apartments});
+    app.get('/', async function(req, res)
+     {
+        await Apartment.find({}, function(err, apartments) {
+            res.render("index", {pagetitle: "Home", path: "home", apartments: apartments});
+        });
     });
 
     app.get('/logout', function(req, res) {
@@ -78,7 +27,7 @@ module.exports = function(app, express) {
 
     app.get("/insert", function(req,res)
     {
-        res.render("insertApartment");
+        res.render("index",{pagetitle:"Inserimento Appartamento", path:"insertApartment"});
     });
 
 };
