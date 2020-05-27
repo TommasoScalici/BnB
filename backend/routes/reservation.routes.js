b6426c86802f67329463120599c03de792dbda78
GirolamoNobile <girolamon20@gmail.com>
2020-05-27 15:53:40 +0200

    cose a cazzo

--- backend/routes/reservation.routes.js
+++ backend/routes/reservation.routes.js
@@ -2,9 +2,8 @@ module.exports = app => {
     const reservationController = require("../controllers/reservations.controller.js");
     var router = require("express").Router();
 
-    router.post('/reservation/:id', function(req,res){
-        res.render("reservation", reservationController.create);
-    })
+    router.post('/apartment/reservation', reservationController.create);
+    
 
-    app.use('/api/reservation', router);
+    app.use('/api/reservations', router);
 };
