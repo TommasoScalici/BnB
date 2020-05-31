f02fc1026dcf3eeaf05599c485b62f03049f4f82
Tommaso Scalici <tommaso.scalici.1991@gmail.com>
2020-05-31 16:37:39 +0200

    Searchbar fixes, added more font-awesome icon, improved apartment-create view.

--- frontend/scripts/bnb.js
+++ frontend/scripts/bnb.js
@@ -15,13 +15,15 @@ $(document).ready(function () {
     });
 
     // Renderizza la preview dell'immagine profilo che si sta caricando
-    $("#profileImage").change(function () {
+    $("#profile-image-input").change(function () {
         if (this.files && this.files[0]) {
             let reader = new FileReader();
             reader.onload = function (e) {
-                $('#profilePicture').attr('src', e.target.result);
+                $('#profile-image').attr('src', e.target.result);
             }
             reader.readAsDataURL(this.files[0]);
+
+            $("#profile-image-input")
         }
     });
 
@@ -29,8 +31,8 @@ $(document).ready(function () {
     // Gestione alerts per il submit dei form
     $("#form-signin").submit(function(event) {
 
-        $('#alertEmail').fadeOut();
-        $('#alertPass').fadeOut();
+        $('#alert-email').fadeOut();
+        $('#alert-pass').fadeOut();
 
         event.preventDefault(); 
         let form = $(this);
@@ -41,16 +43,16 @@ $(document).ready(function () {
             data: form.serialize()
         }).done(function() {
 
-            $('#alertSignInSuccess').fadeIn(2000).fadeOut(1000);
-            setTimeout(() => { $('#signinModal').modal('hide'); }, 3000);
+            $('#alert-signin-success').fadeIn(2000).fadeOut(1000);
+            setTimeout(() => { $('#signin-modal').modal('hide'); }, 3000);
             setTimeout(() => { window.location.replace(window.location.pathname); }, 4000);
 
         }).fail(function(response) {
 
             if(response.responseJSON.message == "User not found")
-                $('#alertEmail').fadeIn(1000);
+                $('#alert-email').fadeIn(1000);
             else if(response.responseJSON.message == "Password mismatch")
-                $('#alertPass').fadeIn(1000);
+                $('#alert-pass').fadeIn(1000);
 
         });
     });
@@ -73,7 +75,7 @@ $(window).on('load', function() {
                 event.stopPropagation();
             else
             {
-                $('#alertSignUpError').fadeOut();
+                $('#alert-signup-error').fadeOut();
  
                 if(form[0].enctype == "multipart/form-data") {
                     var formData = new FormData($(this)[0]);
@@ -86,12 +88,12 @@ $(window).on('load', function() {
                         contentType: false,
                         processData: false,
                         success: function(response) {
-                            $('#alertSignUpSuccess').fadeIn(2000).fadeOut(1000);
+                            $('#alert-signup-success').fadeIn(2000).fadeOut(1000);
                             setTimeout(() => { window.location.replace("/"); }, 3000)
                         },
                         error: function(response) {
-                            $('#alertSignUpError').html(response.responseJSON.message);
-                            $('#alertSignUpError').fadeIn(1000);
+                            $('#alert-signup-error').html(response.responseJSON.message);
+                            $('#alert-signup-error').fadeIn(1000);
                         }
                     });
                 }
@@ -103,13 +105,13 @@ $(window).on('load', function() {
                         data: form.serialize(),
                         success: function() {
             
-                            $('#alertSignUpSuccess').fadeIn(2000).fadeOut(1000);
+                            $('#alert-signup-success').fadeIn(2000).fadeOut(1000);
                             setTimeout(() => { window.location.replace("/"); }, 3000);
                         },
                         error: function(response) {
             
-                            $('#alertSignUpError').html(response.responseJSON.message);
-                            $('#alertSignUpError').fadeIn(1000);
+                            $('#alert-signup-error').html(response.responseJSON.message);
+                            $('#alert-signup-error').fadeIn(1000);
                 
                         }
                     });
