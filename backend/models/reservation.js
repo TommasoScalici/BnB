a02dda6fec2d9b7dae02e05ed0a8973db16269dd
GirolamoNobile <girolamon20@gmail.com>
2020-06-06 16:34:09 +0200

    some little fixes

--- backend/models/reservation.js
+++ backend/models/reservation.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
+const User = require('./user');
+const Apartment = require('./apartment');
+const Schema = mongoose.Schema;
 
 var ReservationSchema = new mongoose.Schema({
 
     // By Tom: il modello non credo vado fatto referenziando gli id come stringhe,
     // ma credo si debbano referenziare gli Schema
-    user: String,
-    apartment: String,
+    user: {type: Schema.Types.ObjectId, ref: "User"},
+    apartment: { type: Schema.Types.ObjectId, ref: 'Apartment' },
     checkin: Date,
     checkout: Date,
     guests: Number,
