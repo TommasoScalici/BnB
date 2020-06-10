138d8e380a8b904b1240e5062d5991995ffa097a
Tommaso Scalici <tommaso.scalici.1991@gmail.com>
2020-06-10 22:11:16 +0200

    BecomeHost now is working.

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,8 +3,10 @@ module.exports = app => {
   
     var router = require("express").Router();
   
-    // Go to the page for becoming a host
-    router.get("/becomehost", userController.becomeHost);
+    // Go to the page for becoming a host or transform user to a user that can host.
+    router.route("/becomehost")
+          .get(userController.renderBecomeHost)
+          .post(userController.becomeHost);
 
     // Logs out the current User
     router.get("/logout", userController.logout);
