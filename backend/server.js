c199082fd8d4ad5b69badee4b872db232899b3b4
Tommaso Scalici <tommaso.scalici.1991@gmail.com>
2020-06-21 00:59:36 +0200

    removed mailreminder from server.js

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const db = require('./config/db.js');
 const middleware = require('./config/middleware.js');
 const routes = require('./config/routes');
-const mailreminder = require('./config/mail-reminder.js');
-
 
 require('dotenv').config();
 
@@ -11,8 +9,6 @@ const app = express();
 
 middleware(app, express);
 routes(app, express);
-mailreminder(app, express);
-
 
 const url = process.env.MONGODB_URI || db.url;
 
